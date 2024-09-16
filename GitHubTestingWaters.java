927b22171976271c3ff06c1423923c580f9c21c4
BigBootyBass <nikosgrib65@gmail.com>
2024-09-16 11:26:58 +0300

    Create GitHubTestingWaters.java
    new main wooooo

---
+++ GitHubTestingWaters.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubtestingwaters;
+        
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author User
+ */
+public class GitHubTestingWaters {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+     
+        JOptionPane.showMessageDialog(null, "Testing da git!!1!11!!!", "Git hub Testis", 1);
+    }
+    
+}
